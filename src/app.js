23c166c2b40887a6f9c31436cd8f6761f5cadc7b
eugenio matias brave <eugenio_m_brave@hotmail.com>
2023-05-13 23:59:10 -0300

    .

--- src/app.js
+++ src/app.js
@@ -6,10 +6,26 @@ app.use(express.urlencoded({ extended: true }));
 
 const productM = new ProductManager()
 
-const getProducts= productM.getProducts()
-
-console.log(await getProducts)
-
-//const port = 8080;
 
 
+
+const port = 8080;
+
+app.get("/Products", async (req, res) => {
+    res.send( productM.getProducts());
+    
+  });
+ 
+
+  app.get("*", (req, res) => {
+    return res.status(404).json({
+      status: "error",
+      msg: "error esa ruta no existe",
+      data: {},
+    });
+  });
+  
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
